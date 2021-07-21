b4bccbe8f0b231a4744c10804303782e54ce1abb
Ehsan G <ehsangn@protonmail.ch>
2021-07-21 21:01:43 +0430

    improvements

--- config.def.h
+++ config.def.h
@@ -1,20 +1,10 @@
 /* mftp's config */
 
 /* specify a terminal file manager */
-static const char* file_manager[] = { "lfrun", "/home/koonix/phone", NULL };
+static const char* file_manager = "lfrun";
 
 /* name, type, host, port, username, password */
 static const Server servers[] = {
-    { "tina fey", FTP, "192.168.1.15", "2121", "koonix", "letmein" },
-    { "emily blunt", FTP, "192.168.1.20", "2121", "koonix", "letmein" },
-    { "tom cruise", FTP, "192.168.1.25", "2121", "koonix", "letmein" },
-    { "john travolta", FTP, "192.168.1.30", "2121", "koonix", "letmein" },
-    { "chris pratt", FTP, "192.168.1.35", "2121", "koonix", "letmein" },
-    { "emilia clarke", FTP, "192.168.1.40", "2121", "koonix", "letmein" },
-    { "luke smith", FTP, "192.168.1.15", "2121", "koonix", "letmein" },
-    { "ehsan ghorbannezhad", FTP, "192.168.1.20", "2121", "koonix", "letmein" },
-    { "tim pope", FTP, "192.168.1.25", "2121", "koonix", "letmein" },
-    { "christopher nolan", FTP, "192.168.1.30", "2121", "koonix", "letmein" },
-    { "elizabeth olsen", FTP, "192.168.1.35", "2121", "koonix", "letmein" },
-    { "olivia wilde", FTP, "192.168.1.40", "2121", "koonix", "letmein" },
+    { "phone", FTP, "192.168.1.15", "2121", "koonix", "ftp", "~/Mount/ftp", NULL },
+    { "laptop", SSH, "192.168.1.30", NULL, "koonix", NULL, "~/Mount/ftp", "~.ssh/id_laptop" },
 };
